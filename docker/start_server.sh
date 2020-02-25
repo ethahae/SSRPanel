e3e16ddd9a6adfb8e80bcebd7eec49b2383de1e3
ryan <wangrui@baicizhan.com>
2020-02-25 17:23:48 +0800

    build

--- docker/start_server.sh
+++ docker/start_server.sh
@@ -2,5 +2,5 @@
 set -e
 printenv >> /etc/environment
 service cron start
-php artisan queue:work database --queue=default --timeout=60 --sleep=5 --tries=3 &
+runuser -u www-data "php artisan queue:work database --queue=default --timeout=60 --sleep=5 --tries=3 &"
 exec apache2-foreground
